745d3bce56f7ca4f7e5a8c652b50d376f3c8a982
spiderverse22xx-sudo <spiderverse22xx@gmail.com>
2026-08-04 14:57:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+async function enviar(){
 
+    let mensaje = document.getElementById("mensaje").value;
+
+    let respuesta = document.getElementById("respuesta");
+
+    respuesta.innerHTML = "Pensando... 🤖";
+
+    let res = await fetch(
+        "https://shopinng.app.n8n.cloud/webhook/84216a82-db04-4056-87ce-3255af95964b",
+        {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                mensaje: mensaje
+            })
+        }
+    );
+
+    let datos = await res.json();
+
+    respuesta.innerHTML = datos.respuesta;
+}
